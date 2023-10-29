0831074fffe60c2fb2285e1e941e7c8082a43186
mkhajiit <mkhajiit15@gmail.com>
2023-10-29 22:55:17 +0900

    practice_app.use

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 //서버구현
+const express = require('express');
+const app = express();
+
+app.use((req, res) => {
+    res.send('<h1>I GOT YOUR RESPONSE!!!<h1>');
+});
+
+app.listen(3000, () => {
+    console.log('LISTENING ON PORT 3000!!!');
+});
